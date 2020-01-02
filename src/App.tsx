72aa58a6d7265de453a8488239d52e7327dc8361
Alex Anthony <alex.anthony28991@gmail.com>
2020-01-02 17:13:24 +0000

    Extend TabBar to handle variable tab numbers

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Pager from './Pager'
 import RadioIcon from '@material-ui/icons/Radio'
 import ComputerIcon from '@material-ui/icons/Computer'
 import TvIcon from '@material-ui/icons/Tv'
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid'
 import { TabShape } from './types'
 
 const tabs: Array<TabShape> = [
   { icon: <RadioIcon />, content: <h1>Radio</h1>, id: 'radio' },
   { icon: <ComputerIcon />, content: <h1>Computer</h1>, id: 'computer' },
   { icon: <TvIcon />, content: <h1>TV</h1>, id: 'tv' },
+  { icon: <PhoneAndroidIcon />, content: <h1>Phone</h1>, id: 'phone' },
 ]
 
 const App: React.FC = () => {
--- src/TabBar.tsx
+++ src/TabBar.tsx
@@ -5,30 +5,36 @@ import { TabShape } from './types'
 import Tab from './Tab'
 import { OpaqueInterpolation, animated } from 'react-spring'
 
-const row = css`
+type TabCountProp = {
+  tabCount: number
+}
+
+const tabBarSize = css<TabCountProp>`
+  height: ${TABBAR_HEIGHT}px;
+  width: ${props => props.tabCount * TAB_WIDTH}px;
+`
+const row = css<TabCountProp>`
+  ${tabBarSize};
   background-color: aqua;
   color: black;
   display: flex;
-  height: ${TABBAR_HEIGHT}px;
-  width: ${TAB_WIDTH * 3}px;
   border-radius: 5px;
   position: absolute;
 `
 
-const Row = styled.div`
+const Row = styled.div<TabCountProp>`
   ${row};
 `
 
-const OverlayRow = styled(animated.div)`
+const OverlayRow = styled(animated.div)<TabCountProp>`
   ${row};
   background-color: palevioletred;
   color: white;
 `
 
-const TabRowContainer = styled.div`
+const TabRowContainer = styled.div<TabCountProp>`
   position: relative;
-  height: ${TABBAR_HEIGHT}px;
-  width: ${TAB_WIDTH * 3}px;
+  ${tabBarSize};
 `
 
 type Props = {
@@ -41,26 +47,28 @@ const TabBar: React.FC<Props> = ({
   activeTabIndex,
   setActiveTabIndex,
 }) => {
+  const tabCount = tabs.length
+  const maxTabIndex = tabCount - 1
   const overlayStyle = {
     clipPath: activeTabIndex.interpolate({
-      range: [0, 2],
+      range: [0, maxTabIndex],
       output: [
-        `inset(0 ${2 * TAB_WIDTH}px 0 0px)`,
-        `inset(0 0px 0 ${2 * TAB_WIDTH}px)`,
+        `inset(0 ${maxTabIndex * TAB_WIDTH}px 0 0px)`,
+        `inset(0 0px 0 ${maxTabIndex * TAB_WIDTH}px)`,
       ],
     }),
   }
 
   return (
-    <TabRowContainer>
-      <Row>
+    <TabRowContainer tabCount={tabCount}>
+      <Row tabCount={tabCount}>
         {tabs.map((tab, i) => (
           <Tab key={tab.id} onClick={() => setActiveTabIndex(i)}>
             {tab.icon}
           </Tab>
         ))}
       </Row>
-      <OverlayRow style={overlayStyle}>
+      <OverlayRow style={overlayStyle} tabCount={tabCount}>
         {tabs.map(tab => (
           <Tab key={tab.id}>{tab.icon}</Tab>
         ))}
